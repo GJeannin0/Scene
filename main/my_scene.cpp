aba5709f3079d4b5c5c50a8b3b5c7fa984ece904
GJeannin0 <j6.guillaume@gmail.com>
2021-07-07 07:42:03 +0200

    model dispalyed

--- main/my_scene.cpp
+++ main/my_scene.cpp
@@ -8,6 +8,7 @@
 #include "engine.h"
 #include "shader.h"
 #include "../build/model.h"
+#include "camera.h"
 
 
 namespace gl {
@@ -31,6 +32,16 @@ namespace gl {
         unsigned int vertex_shader_;
         unsigned int fragment_shader_;
         unsigned int program_;
+
+        std::unique_ptr<Camera> camera_ = nullptr;
+    	
+        glm::mat4 model_ = glm::mat4(1.0f);
+        glm::mat4 view_ = glm::mat4(1.0f);
+        glm::mat4 projection_ = glm::mat4(1.0f);
+
+
+
+    	
         void IsError(const std::string& file, int line);
     };
 
@@ -49,15 +60,16 @@ namespace gl {
 
     void MyScene::Init()
     {
-        
-        model.LoadModel("..\\data\\meshes\\nanosuit.obj");
+        glEnable(GL_DEPTH_TEST);
+        camera_ = std::make_unique<Camera>(glm::vec3(.0f, 8.0f, 20.0f));
+        model.LoadModel("data/meshes/nanosuit.obj");
 
         std::string path = "..\\";
 
         std::string ifs_verts(
-            path + "data\\shaders\\hello_triangle\\triangle.vert");
+            path + "data/shaders/model/model.vert");
         std::string ifs_frags(
-            path + "data\\shaders\\hello_triangle\\triangle.frag");
+            path + "data/shaders/model/model.frag");
 
         shader_ = Shader(ifs_verts, ifs_frags);
 
@@ -65,9 +77,9 @@ namespace gl {
 
     	
         std::ifstream ifs_vert(
-            path + "data\\shaders\\hello_triangle\\triangle.vert");
+            path + "data\\shaders\\model\\model.vert");
         std::ifstream ifs_frag(
-            path + "data\\shaders\\hello_triangle\\triangle.frag");
+            path + "data\\shaders\\model\\model.frag");
     	
         if (!ifs_vert.is_open())
         {
@@ -135,6 +147,18 @@ namespace gl {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);       
         glUseProgram(program_);
 
+        view_ = camera_->GetViewMatrix();
+
+		projection_ = glm::perspective(
+			glm::radians(45.0f), 
+			4.0f / 3.0f, 
+			0.1f, 
+			100.f); 	
+        shader_.SetMat4("model", model_);
+        shader_.SetMat4("view", view_);
+        shader_.SetMat4("projection", projection_);
+
+    	
         model.DrawModel(shader_);
     }
 
